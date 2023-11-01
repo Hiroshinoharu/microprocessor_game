985f96092ba63ff38e908a82816b938cabbd25d2
Jenn128 <149515613+Jenn128@users.noreply.github.com>
2023-11-01 10:49:19 +0000

    Update main.c

--- graphics_demo/src/main.c
+++ graphics_demo/src/main.c
@@ -20,6 +20,7 @@ void setupIO();
 int isInside(uint16_t x1, uint16_t y1, uint16_t w, uint16_t h, uint16_t px, uint16_t py);
 void enablePullUp(GPIO_TypeDef *Port, uint32_t BitNumber);
 void pinMode(GPIO_TypeDef *Port, uint32_t BitNumber, uint32_t Mode);
+void initSound();
 
 
 //Signature to render the game's textures
